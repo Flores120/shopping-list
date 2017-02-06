991cfdfd6ac6094c17e289fd28b59c174ececc2c
Alvaro Flores <alvaro.flores01@gmail.com>
2017-02-06 11:36:51 -0800

    adds fuctionality to website

--- show-list.php
+++ show-list.php
@@ -1,5 +1,12 @@
 <?php
-    $groceries = array("eggs", "milk", "bread");
+    $eggs = $_GET["eggs"];
+    $milk = $_GET["milk"];
+    $cheese = $_GET["cheese"];
+    $ham = $_GET["ham"];
+    $bread = $_GET["bread"];
+
+    $groceries = array($eggs, $milk, $cheese, $ham, $bread);
+
  ?>
 <!DOCTYPE html>
 <html>
@@ -9,12 +16,13 @@
   </head>
   <body>
     <h1>My Gorcery List</h1>
-    <ul>
-      <?php
-          foreach ($groceries as $purchase) {
-            echo "<li>" . $purchase . "</li>";
-          }
-       ?>
-    </ul>
+    <?php
+    $grocery_price = 0;
+        foreach ($groceries as $purchase) {
+          echo "<li>" .  "The total price so far is: $purchase" . "</li>";
+          $grocery_price = $grocery_price + $purchase;
+        }
+        echo "<h2>" . "Your total price is: $grocery_price" . "</h2>";
+     ?>
   </body>
 </html>
